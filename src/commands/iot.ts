41630c16c84856c3b3dc4fc1bd3695cf2f85e347
yhattm <yhattm@gmail.com>
2022-09-20 12:25:10 +0000

    add rxjs

--- src/commands/iot.ts
+++ src/commands/iot.ts
@@ -1,4 +1,5 @@
-import {Command, Flags} from '@oclif/core'
+import { Command, Flags } from '@oclif/core'
+import { timer, interval } from 'rxjs';
 
 export default class Iot extends Command {
   static description = 'describe the command here'
@@ -9,20 +10,23 @@ export default class Iot extends Command {
 
   static flags = {
     // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
+    name: Flags.string({ char: 'n', description: 'name to print' }),
     // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
+    force: Flags.boolean({ char: 'f' }),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{ name: 'file' }]
 
   public async run(): Promise<void> {
-    const {args, flags} = await this.parse(Iot)
+    const { args, flags } = await this.parse(Iot)
 
     const name = flags.name ?? 'world'
+    this.log('Start')
     this.log(`hello ${name} from /workspaces/weizhen/src/commands/iot.ts`)
     if (args.file && flags.force) {
       this.log(`you input --force and --file: ${args.file}`)
     }
+
+    timer(0, 5000).subscribe(n => this.log('keep alive', n));
   }
 }
